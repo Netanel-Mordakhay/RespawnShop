dae49e3b9c0496231c18bd8f93b7d2204d1734c9
Netanel <mordakhay.netanel@gmail.com>
2024-12-07 01:09:50 +0200

    ViewImageScreen icons update

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <ViewImageScreen />;
 }
