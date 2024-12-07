160f84eb5c6b8529a194bf42f693d30ede4ab2ec
Netanel <mordakhay.netanel@gmail.com>
2024-12-07 16:11:02 +0200

    AppPicker + Picketitem built and updated

--- App.js
+++ App.js
@@ -1,24 +1,40 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { View, Text, StyleSheet, TextInput, Switch } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
+
+const categories = [
+  {
+    label: "Playstation",
+    value: 1,
+  },
+  {
+    label: "Xbox",
+    value: 2,
+  },
+  {
+    label: "PC",
+    value: 3,
+  },
+];
 
 export default function App() {
-  const [firstName, setFirstName] = useState("");
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <Screen>
-        <AppTextInput placeholder="username" icon="email" />
+        <AppPicker
+          selectedItem={category}
+          onSelectItem={(item) => setCategory(item)}
+          items={categories}
+          icon="apps"
+          placeholder="category"
+        />
+        <AppTextInput icon="email" placeholder="email" />
       </Screen>
     </GestureHandlerRootView>
   );
