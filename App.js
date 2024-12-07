e8d88edd714efb1f7364f26b8342fc555c247f47
Netanel <mordakhay.netanel@gmail.com>
2024-12-07 15:42:02 +0200

    Deafult styles updated

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import ViewImageScreen from "./app/screens/ViewImageScreen";
@@ -10,11 +10,16 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
+  const [firstName, setFirstName] = useState("");
+
   return (
     <GestureHandlerRootView style={styles.container}>
-      <ListingsScreen />
+      <Screen>
+        <AppTextInput placeholder="username" icon="email" />
+      </Screen>
     </GestureHandlerRootView>
   );
 }
