1f4d7f291d38d8b4bb19f24b45da82a845c505d3
Netanel <mordakhay.netanel@gmail.com>
2024-12-07 18:02:34 +0200

    LoginScreen + ErrorMessage + Formik & yup form

--- App.js
+++ App.js
@@ -5,37 +5,12 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Screen from "./app/components/Screen";
 import AppTextInput from "./app/components/AppTextInput";
 import AppPicker from "./app/components/AppPicker";
-
-const categories = [
-  {
-    label: "Playstation",
-    value: 1,
-  },
-  {
-    label: "Xbox",
-    value: 2,
-  },
-  {
-    label: "PC",
-    value: 3,
-  },
-];
+import LoginScreen from "./app/screens/LoginScreen";
 
 export default function App() {
-  const [category, setCategory] = useState(categories[0]);
-
   return (
     <GestureHandlerRootView style={styles.container}>
-      <Screen>
-        <AppPicker
-          selectedItem={category}
-          onSelectItem={(item) => setCategory(item)}
-          items={categories}
-          icon="apps"
-          placeholder="category"
-        />
-        <AppTextInput icon="email" placeholder="email" />
-      </Screen>
+      <LoginScreen />
     </GestureHandlerRootView>
   );
 }
