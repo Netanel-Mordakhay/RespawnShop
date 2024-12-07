48267676c9fcf7be57bad01dee37f9a2953d9692
Netanel <mordakhay.netanel@gmail.com>
2024-12-08 00:31:04 +0200

    RegisterScreen name validation added

--- app/screens/RegisterScreen.js
+++ app/screens/RegisterScreen.js
@@ -8,6 +8,7 @@ import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
   password: Yup.string().required().min(4).label("Password"),
+  name: Yup.string().required().min(3).label("Name"),
 });
 
 function RegisterScreen(props) {
